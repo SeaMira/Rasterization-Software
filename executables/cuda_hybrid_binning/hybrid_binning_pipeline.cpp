a3f53df73866012b3708bd8c6ca6dc85fbc777f4
Sebastian Mira Pacheco <sebastianandresmira@gmail.com>
2026-05-21 19:22:25 -0400

    ooc frame times

--- executables/cuda_hybrid_binning/hybrid_binning_pipeline.cpp
+++ executables/cuda_hybrid_binning/hybrid_binning_pipeline.cpp
@@ -158,9 +158,9 @@ public:
         launchScreenClear(outputImage, d_depthBuffer, m_config.screenWidth, m_config.screenHeight, camera.getFar(), stream);
         nvtxRangePop();
 
-        nvtxRangePushA("Sphere Pipeline Binning");
-        executeSpherePipelineBinning(d_spheres, m_sphereCount, d_depthBuffer, outputImage, &m_sphereResources, stream);
-        nvtxRangePop();
+        // nvtxRangePushA("Sphere Pipeline Binning");
+        // executeSpherePipelineBinning(d_spheres, m_sphereCount, d_depthBuffer, outputImage, &m_sphereResources, stream);
+        // nvtxRangePop();
 
         nvtxRangePushA("Cylinder Pipeline Binning");
         executeCylinderPipelineBinning(d_cylinders, d_spheres, m_cylinderCount, d_depthBuffer, outputImage, &m_cylinderResources, stream);
